6fb7ad817ce672f8f8e3199e247899aaa6cd2667
Nicolas Savaris <nicosavaris.dev@gmail.com>
2026-08-14 20:01:16 +0200

    Add weather search handling

--- 1.2_weather_app/JS/script.js
+++ 1.2_weather_app/JS/script.js
@@ -2,13 +2,16 @@ const searchForm = document.querySelector("#search-form");
 const cityInput = document.querySelector("#city-input");
 
 searchForm.addEventListener("submit", (event) => {
+
     event.preventDefault();
+    console.log("Form submitted");
 
     const city = cityInput.value.trim();
+    console.log(city);
 
     if (!city) {
         return;
     }
 
-    console.log(`Searching for: ${city}`);
+    getWeather();
 });
