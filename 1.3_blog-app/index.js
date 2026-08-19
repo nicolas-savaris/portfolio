bf2d33aa41a070a6421c5618832a710ec3e6468b
Nicolas Savaris <nicosavaris.dev@gmail.com>
2026-08-19 10:11:18 +0200

    Handle new blog posts

--- 1.3_blog-app/index.js
+++ 1.3_blog-app/index.js
@@ -3,21 +3,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-
 app.set('view engine', 'ejs');
-
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Sample route
-/*app.get('/', (req, res) => {
-    res.render('index');
-    
-});
-*/
 app.get('/', (req,res) => {
     res.render('index', { posts });
+});
+
+app.post('/post', (req, res) => {
+    // qui recupereremo i dati del form
+    console.log(req.body['post-title']);
+    console.log(req.body['post-content']);
+    
 });
 
 // Start the server
